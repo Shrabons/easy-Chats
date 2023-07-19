4fe54a8eae84eaf95c52d39d05bfcb72d03a3309
shrabons <shrabon.developer@gamil.com>
2023-07-19 21:55:09 +0600

    sms send and recive woriking

--- src/components/chats/Chats.js
+++ src/components/chats/Chats.js
@@ -1,31 +1,44 @@
+import EmojiPicker from 'emoji-picker-react';
 import { getDatabase, onValue, push, ref, set } from "firebase/database";
+import { getDownloadURL, getStorage, ref as sref, uploadBytes, uploadBytesResumable, uploadString } from "firebase/storage";
+import moment from "moment/moment";
 import React, { useEffect, useState } from 'react';
+import { AudioRecorder } from 'react-audio-voice-recorder';
 import Camera, { FACING_MODES, IMAGE_TYPES } from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
-import { AiOutlineClose } from 'react-icons/ai';
-import { BsCameraFill, BsFillMicFill, BsFillSendFill, BsThreeDotsVertical, BsTriangleFill } from 'react-icons/bs';
+import { AiFillDelete, AiOutlineClose } from 'react-icons/ai';
+import { BsCameraFill, BsFillEmojiSmileFill, BsFillSendFill, BsThreeDotsVertical, BsTriangleFill } from 'react-icons/bs';
 import { GrGallery } from 'react-icons/gr';
+import ModalImage from "react-modal-image";
 import { useSelector } from 'react-redux';
 
 
 
 const Chats = () => {
 
+
+    const storage = getStorage();
+
     const db = getDatabase();
     let [camerashow , setCamerShow] = useState(false)
     let [captureimg , setCaptureimg] = useState("")
     let [msgsingle , setMsgsingle] = useState("")
     let [msglist , setMsglist] = useState([])
+    let [audioUrl , setAudioUrl] = useState("")
+    let [blob , setBlob] = useState("")
+    let [emojishow , setEmojishow] = useState(false)
+
 
     const data = useSelector((state) => state.reducer.userInfo)
     const activeChatName = useSelector((state) => state.reducer.active)
-    console.log(activeChatName)
+   
 
    
    
 
     let hanldemsgCase = (e) => {
         setMsgsingle(e.target.value)
+        
     }
     
     
@@ -33,21 +46,44 @@ const Chats = () => {
         // Do stuff with the photo...
         console.log('takePhoto');
         setCaptureimg(dataUri);
+        const storageRef = sref(storage,"thik ace ki nah");
+        uploadString(storageRef, dataUri, 'data_url').then((snapshot) => {
+            console.log('Uploaded a data_url string!');
+            getDownloadURL(storageRef).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+                set(push(ref(db, 'singlemsg/')), {
+                    whosendid: data.uid,
+                    whosendname: data.displayName,
+                    whoreciveid: activeChatName.id,
+                    whorecivename: activeChatName.name,
+                    img: downloadURL,
+                    date: `${new Date().getFullYear()} ${new Date().getMonth()+1} ${new Date().getDate()} ${new Date().getHours()} : ${new Date().getMinutes()}`,
+                  }).then(()=>{
+                    setCamerShow(false)
+                  })
+                
+            });
+        });
       }
     
 
 
       let handleMsgSend = () =>{
         if(activeChatName.status == "single"){
-            set(push(ref(db, 'singlemsg/')), {
-                whosendid: data.uid,
-                whosendname: data.displayName,
-                whoreciveid: activeChatName.id,
-                whorecivename: activeChatName.name,
-                msg: msgsingle,
-                date: `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()} ${new Date().getHours()} : ${new Date().getMinutes()}`
-              });
-              setMsgsingle("")
+            if(!msgsingle){
+                console.log("Enter the message !")
+            }else{
+                set(push(ref(db, 'singlemsg/')), {
+                    whosendid: data.uid,
+                    whosendname: data.displayName,
+                    whoreciveid: activeChatName.id,
+                    whorecivename: activeChatName.name,
+                    msg: msgsingle,
+                    date: `${new Date().getFullYear()}-${new Date().getMonth()+1}-${new Date().getDate()} ${new Date().getHours()} : ${new Date().getMinutes()}`
+                  });
+                  setMsgsingle("")
+            }
+            
             
         }else{
             console.log( "ami group msg theke aseci")
@@ -69,9 +105,83 @@ const Chats = () => {
         });
     },[activeChatName.id])
 
-    let handleImgChanges = (e) =>{
-        console.log(e.target)
+
+// img file updating  
+    let handleImgUpChanges = (e) =>{
+        console.log(e.target.files[0])
+        if(!e.target.files[0]){
+            console.log("your file not a select !")
+        }else{
+            const storageRef = sref(storage, e.target.files[0].name);
+
+            const uploadTask = uploadBytesResumable(storageRef, e.target.files[0]);
+            uploadTask.on('state_changed', 
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                
+                
+            }, 
+            (error) => {
+                console.log(error)
+            }, 
+            () => {
+                // Handle successful uploads on complete
+                // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);  
+                set(push(ref(db, 'singlemsg/')), {
+                    whosendid: data.uid,
+                    whosendname: data.displayName,
+                    whoreciveid: activeChatName.id,
+                    whorecivename: activeChatName.name,
+                    img: downloadURL,
+                    date: `${new Date().getFullYear()} ${new Date().getMonth()+1} ${new Date().getDate()} ${new Date().getHours()} : ${new Date().getMinutes()}`,
+                  });
+                });
+            }
+            );
+        }
+        
     }
+
+
+// recoding function working 
+const addAudioElement = (blob) => {
+    const url = URL.createObjectURL(blob);
+    setAudioUrl(url)
+    setBlob(blob)
+
+  };
+
+//   audio submit data base function working 
+
+ let handleAudioUpdate = () =>{
+    const audioStorageRef = sref(storage, audioUrl);
+
+    // 'file' comes from the Blob or File API
+    uploadBytes(audioStorageRef, blob).then((snapshot) => {
+        getDownloadURL(audioStorageRef).then((downloadURL) => {
+            console.log('Audio url', downloadURL);
+            set(push(ref(db, 'singlemsg/')), {
+                whosendid: data.uid,
+                whosendname: data.displayName,
+                whoreciveid: activeChatName.id,
+                whorecivename: activeChatName.name,
+                audio: downloadURL,
+                date: `${new Date().getFullYear()} ${new Date().getMonth()+1} ${new Date().getDate()} ${new Date().getHours()} : ${new Date().getMinutes()}`,
+              }).then(()=>{
+                setAudioUrl("")
+              })
+        });
+  
+    });
+ }
+
+ let handleEmojiCase =(emojis)=>{
+    console.log(emojis.emoji)
+    setMsgsingle(msgsingle+emojis.emoji)
+ }
 
   return (
     <div className='bg-white shadow-lg rounded-xl py-6 px-12 '>
@@ -91,24 +201,61 @@ const Chats = () => {
                 {activeChatName.status == "single"
                 ?
                 msglist.map((item, index)=>(
-                    item.whosendid == data.uid 
-                    ?
-                    
-                    <div className='mb-8 text-right'>
-                        <div className='bg-primary inline-block py-3 px-10 rounded-md relative mx-5'>
-                            <p className='font-pop font-medium text-base text-white text-left'>{item.msg}</p>
-                            <BsTriangleFill className='text-2xl absolute bottom-[-1.4px] right-[-10px]  text-primary' />
-                        </div>
-                        <p className=' mr-5 font-pop ml-5 font-medium text-sm mt-1 text-[rgba(0,0,0,.25)]'>Today, 2:01pm</p>
-                    </div> 
+                    item.whosendid == data.uid ?
+                        item.msg ?
+                        <div className='mb-8 text-right '>
+                            <div className='bg-primary inline-block py-3 px-10 rounded-md relative mx-5'>
+                                <p className='font-pop font-medium text-base text-white text-left'>{item.msg}</p>
+                                <BsTriangleFill className='text-2xl absolute bottom-[-1.4px] right-[-10px]  text-primary' />
+                            </div>
+                            <p className=' mr-5 font-pop ml-5 font-medium text-sm mt-1  text-[rgba(0,0,0,.25)]'>{moment(item.date, "YYYYMMDD hh:mm").fromNow()}</p>
+                        </div> 
+                        : 
+                        item.img ?
+                        <div className='mb-8 text-right'>
+                            <div className=' inline-block w-72 rounded-md relative mx-5'>
+                                <ModalImage className='rounded-md'
+                                small={item.img}
+                                large={item.img}
+                                />
+                            </div>
+                            <p className=' mr-5 font-pop ml-5 font-medium text-sm  text-[rgba(0,0,0,.25)]'>{moment(item.date, "YYYYMMDD hh:mm").fromNow()}</p>
+                        </div> 
+                        :
+                        <div className='mb-8 text-right'>
+                            <div className=' inline-block w-72 rounded-md relative mx-5'>
+                                <audio controls src={item.audio}></audio>
+                            </div>
+                            <p className=' mr-5 font-pop ml-5 font-medium text-sm  text-[rgba(0,0,0,.25)]'>{moment(item.date, "YYYYMMDD hh:mm").fromNow()}</p>
+                        </div> 
                     :
-                    <div className='mb-8'>
-                        <div className='bg-[#F1F1F1] inline-block py-3 px-10 rounded-md relative mx-5'>
-                            <p className='font-pop font-medium text-base text-black'>{item.msg}</p>
-                            <BsTriangleFill className='text-2xl absolute bottom-[-1.5px] left-[-10px]  text-[#F1F1F1] ' />
-                        </div>
-                        <p className='font-pop ml-5 font-medium text-sm mt-1 text-[rgba(0,0,0,.25)]'>Today, 2:01pm</p>
-                    </div> 
+                        item.msg ?
+                        <div className='mb-8'>
+                            <div className='bg-[#F1F1F1] inline-block py-3 px-10 rounded-md relative mx-5'>
+                                <p className='font-pop font-medium text-base text-black'>{item.msg}</p>
+                                <BsTriangleFill className='text-2xl absolute bottom-[-1.5px] left-[-10px]  text-[#F1F1F1] ' />
+                            </div>
+                            <p className='font-pop ml-5 font-medium text-sm mt-1 text-[rgba(0,0,0,.25)]'>{moment(item.date, "YYYYMMDD hh:mm").fromNow()}</p>
+                        </div> 
+                        :
+                        item.img ?
+                        <div className='mb-8'>
+                            <div className=' inline-block  w-72 rounded-md relative mx-5'>
+                                <ModalImage className='rounded-md'
+                                small={item.img}
+                                large={item.img}
+                                />
+                            </div>
+                            <p className='font-pop ml-5 font-medium text-sm  text-[rgba(0,0,0,.25)]'>{moment(item.date, "YYYYMMDD hh:mm").fromNow()}</p>
+                        </div> 
+                        :
+                        <div className='mb-8'>
+                            <div className=' inline-block  w-72 rounded-md relative mx-5'>
+                                <audio controls src={item.audio}></audio>
+                            </div>
+                            <p className='font-pop ml-5 font-medium text-sm  text-[rgba(0,0,0,.25)]'>{moment(item.date, "YYYYMMDD hh:mm").fromNow()}</p>
+                        </div> 
+                    
                 ))
                 :
                 <h1>this is a group sms</h1>
@@ -214,14 +361,54 @@ const Chats = () => {
         </div>
         <div className=' flex mt-8 gap-x-5'>
             <div className="w-[90%] relative">
-                <input value={msgsingle} onChange={hanldemsgCase} type="text" className='bg-[#f1f1f1] p-3  w-full rounded-lg'  />
-                <label>
-                    <input onChange={handleImgChanges} className='hidden' type="file" />
-                    <GrGallery className='absolute top-4 right-3' />
-                </label>
-                <BsCameraFill onClick={()=>setCamerShow(!camerashow)} className='absolute top-4 right-10 text-lg pointer' />
-                <BsFillMicFill  className='absolute top-4 right-[72px]  ' />
+                {!audioUrl && 
+                <>
+                    <input value={msgsingle} onChange={hanldemsgCase} type="text" className='bg-[#f1f1f1] p-3  w-full rounded-lg'  />
+                    <label>
+                        <input onChange={handleImgUpChanges} className='hidden' type="file" />
+                        <GrGallery className='absolute top-4 right-3' />
+                    </label>
+                    <BsCameraFill onClick={()=>setCamerShow(!camerashow)} className='absolute top-4 right-10 text-lg pointer' />
+                    <BsFillEmojiSmileFill onClick={()=> setEmojishow(!emojishow)} className='absolute top-4 right-[95px] ' />
+                    {emojishow &&
+                        <div className='absolute top-[-456px] right-0'>
+                            <EmojiPicker onEmojiClick={(emojis)=>handleEmojiCase(emojis)} />
+                        </div>
+                    }
+                    
+                    <AudioRecorder
+                        onRecordingComplete={addAudioElement}
+                        audioTrackConstraints={{
+                        noiseSuppression: true,
+                        echoCancellation: true,
+                        // autoGainControl,
+                        // channelCount,
+                        // deviceId,
+                        // groupId,
+                        // sampleRate,
+                        // sampleSize,
+                        }}
+                        onNotAllowedOrFound={(err) => console.table(err)}
+                        downloadOnSavePress={false}
+                        downloadFileExtension="webm"
+                        mediaRecorderOptions={{
+                        audioBitsPerSecond: 128000,
+                        }}
+                        // showVisualizer={true}
+                    />
+                </>
                 
+                }
+                
+                {audioUrl && 
+
+                <div className=" flex gap-x-5 justify-end">
+                    <audio controls src={audioUrl}></audio>
+                    <button onClick={()=> setAudioUrl("")} className='bg-red-400 py-3 px-6 rounded-lg '><AiFillDelete className='text-2xl text-white' /></button>
+                    <button onClick={handleAudioUpdate} className='bg-primary py-3 px-6 rounded-lg '><BsFillSendFill className='text-2xl text-white' /></button>
+
+                </div>
+                }
                 
             </div>
             {camerashow &&
@@ -242,7 +429,9 @@ const Chats = () => {
                         />
                     </div>
                 }
+            {!audioUrl &&
             <button onClick={handleMsgSend} className='bg-primary py-3 px-6 rounded-lg '><BsFillSendFill className='text-2xl text-white' /></button>
+            }
         </div>
     </div>
   )
