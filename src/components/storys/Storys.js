41a94234fe25fb4083aff3c8b1047a17223cbd08
shrabons <shrabon.developer@gamil.com>
2023-08-26 08:35:13 +0600

    change sorys

--- src/components/storys/Storys.js
+++ src/components/storys/Storys.js
@@ -43,30 +43,7 @@ const Storys = () => {
                 <span className='text-white font-nunito font-medium '>{data.displayName}</span>
             </div>
         </div>
-        <div className='crate-story h-48 bg-slate-700 w-1/4 rounded-lg overflow-hidden relative'>
-            <div className=''>
-                <img className='w-full h-48' src="images/bg-right.png"alt="" />
-                <div className='absolute top-0 left-0 bg-black w-full h-full opacity-50'></div>
-            </div>
-            <div className='w-8 h-8 bg-slate-500 absolute top-2 right-2 rounded-full overflow-hidden border-[3px] border-primary '>
-                <img className='w-full' src={data.photoURL} alt="" />
-            </div>
-            <div className='absolute bottom-0 left-0 w-full text-center py-3'>
-                <span className='text-white font-nunito font-medium '>{data.displayName}</span>
-            </div>
-        </div>
-        <div className='crate-story h-48 bg-slate-700 w-1/4 rounded-lg overflow-hidden relative'>
-            <div className=''>
-                <img className='w-full h-48' src="images/bg-right.png"alt="" />
-                <div className='absolute top-0 left-0 bg-black w-full h-full opacity-50'></div>
-            </div>
-            <div className='w-8 h-8 bg-slate-500 absolute top-2 right-2 rounded-full overflow-hidden border-[3px] border-primary '>
-                <img className='w-full' src={data.photoURL} alt="" />
-            </div>
-            <div className='absolute bottom-0 left-0 w-full text-center py-3'>
-                <span className='text-white font-nunito font-medium '>{data.displayName}</span>
-            </div>
-        </div>
+       
         
        
     </div>
